b47a44ee45c6d1b4a3449ba60df78ddd95753c4a
AnushaGiri417 <anushagiri417@gmail.com>
2021-07-22 11:12:07 -0400

    jenkinsPractice first commit

---
+++ src/Jenkins/anushaJenkins.java
@@ -0,0 +1,9 @@
+package Jenkins;
+
+public class anushaJenkins {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.println("Hello world");
+        }
+    }
+}
