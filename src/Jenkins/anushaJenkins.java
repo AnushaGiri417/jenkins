4d3c5d838b559afad9af225ad6b0a7911804eeb4
AnushaGiri417 <anushagiri417@gmail.com>
2021-07-22 11:50:07 -0400

    new commit third

--- src/Jenkins/anushaJenkins.java
+++ src/Jenkins/anushaJenkins.java
@@ -4,7 +4,7 @@ public class anushaJenkins {
 
     public static void main(String[] args) {
 
-        for(int i=0; i<5; i++){
+        for(int i=0; i<3; i++){
 
             System.out.println("Changed code again");
         }
